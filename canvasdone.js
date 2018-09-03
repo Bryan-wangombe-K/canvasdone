8d46875b6f691a5ac89d4145520c1c3fb646e5ec
Brian Wang'ombe <brianwangombe@gmail.com>
2018-09-03 04:09:59 +0300

    added a fillRect at 0,0

--- canvasdone.js
+++ canvasdone.js
@@ -2,4 +2,6 @@ window.onload = function() {
   var canvas = document.getElementById('canvas');
   var c = canvas.getContext('2d')
   c.fillRect(150,150,50,50)
+  c.beginPath()
+  c.fillRect(0,0,50,50)
 }
